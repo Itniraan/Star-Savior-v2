0b4d7dc9df81ab70616a62e429231ce159b7b887
mallory13 <mallory.steele13@gmail.com>
2014-11-27 11:38:50 -0500

    Added functions and different backgrounds to level 2 and level 3

--- Star_Savior_V2/js/states/playGameLevelTwoScreen.ts
+++ Star_Savior_V2/js/states/playGameLevelTwoScreen.ts
@@ -1,9 +1,71 @@
-﻿module states {
+﻿/// <reference path="../constants.ts" />
+/// <reference path="../objects/plane.ts" />
+/// <reference path="../objects/enemy.ts" />
+/// <reference path="../objects/star.ts" />
+/// <reference path="../objects/bullet.ts" />
+/// <reference path="../objects/lava.ts" />
+/// <reference path="../managers/collision.ts" />
+/// <reference path="../objects/scoreboard.ts" />
+/**
+File Name: playGameLevelTwo.ts
+Author: Blake Murdock and Mallory Steele
+Website Name: This is the game play screen state for Star Savior
+Purpose: This file contains all of the elements of the game play screen
+*/
+module states {
+    // Game loop function, that makes sure everything runs smoothly
     export function playLevelTwoState() {
+        lava.update();
+        star.update();
 
+        for (var count = 0; count < constants.ENEMY_NUM; count++) {
+            enemies[count].update();
+        }
+        managers.collisionCheck();
+        console.log(managers.collisionCheck());
+        for (var i = 0; i < bullets.length; i++) {
+            bullets[i].bulletUpdate();
+        }
+
+        plane.update();
+        scoreboard.update();
+
+        // If lives is 0 or lower, destroy all objects and go to gameover screen
+        if (scoreboard.lives <= 0) {
+            states.getHighScore(scoreboard.score);
+            stage.removeChild(game);
+            plane.destroy();
+            lava.destroy();
+            star.destroy();
+
+            game.removeAllChildren();
+            game.removeAllEventListeners();
+            stage.removeChild(game);
+
+            currentState = constants.GAME_OVER_STATE;
+            changeState(currentState);
+        }
     }
 
-    export function playLevelTwo() {
+    // play state Function
+    export function playLevelTwo(): void {
+        game = new createjs.Container();
+        // Set mouse cursor to none (avatar will take place of cursor
+        stage.cursor = "none";
+		space = new objects.levelTwoBackground(stage, game);
+        star = new objects.Star(stage, game);
+        plane = new objects.Plane(stage, game);
+
+
+        
+
+        for (var count = 0; count < constants.ENEMY_NUM; count++) {
+            enemies[count] = new objects.Enemy(stage, game);
+        }
+
+        scoreboard = new objects.scoreBoard(stage, game);
+
+        stage.addChild(game);
 
     }
 } 
